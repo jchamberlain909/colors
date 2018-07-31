cde7835559445b017ab037f1edfba64c14826212
jchamberlain909 <jchamberlain909@gmail.com>
2018-07-31 15:32:00 -0400

    testing nginx adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :pixels, except: [:new, :edit, :destroy]
   resources :users, except: [:new, :edit, :destroy]
-  mount SseRailsEngine::Engine, at: '/sse'
+  get '/subscribe', to: 'pixels#subscribe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-SseRailsEngine.access_control_allow_origin = "*"
-
