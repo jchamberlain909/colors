a07acf53469ec93febd1a82a7b0f98ae1b510897
jchamberlain909 <jchamberlain909@gmail.com>
2018-07-30 21:04:55 -0400

    put pg gem back in

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+set :linked_files, %w{config/master.key}
+
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
